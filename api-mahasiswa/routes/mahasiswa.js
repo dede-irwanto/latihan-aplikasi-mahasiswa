d17dc2171e79e090ab14f846036819bd3e2d79c3
Dede Irwanto <52788483+dede-irwanto@users.noreply.github.com>
2022-02-03 06:55:40 +0800

    add post mahasiswa

--- api-mahasiswa/routes/mahasiswa.js
+++ api-mahasiswa/routes/mahasiswa.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const { body, validationResult } = require("express-validator");
 const router = express.Router();
 
 // import database
 const connection = require("../config/database");
 
 /**
- * INDEX MAHASISWA
+ * GET ALL MAHASISWA
  */
 router.get("/", (req, res) => {
   // query
@@ -28,4 +29,49 @@ router.get("/", (req, res) => {
   );
 });
 
+/**
+ * POST MAHASISWA
+ */
+router.post(
+  "/store",
+  [
+    // validation
+    body("nim").notEmpty(),
+    body("nama").notEmpty(),
+    body("jurusan").notEmpty(),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({
+        errors: errors.array(),
+      });
+    }
+
+    // define formData
+    let formData = {
+      nim: req.body.nim,
+      nama: req.body.nama,
+      jurusan: req.body.jurusan,
+    };
+
+    // insert query
+    connection.query("INSERT INTO mahasiswa SET ?", formData, (err, rows) => {
+      // if error throw err
+      if (err) {
+        return res.status(500).json({
+          status: false,
+          message: "Internal Server Error",
+        });
+      } else {
+        return res.status(200).json({
+          status: true,
+          message: "Insert Data Successfully",
+          data: rows[0],
+        });
+      }
+    });
+  }
+);
 module.exports = router;
