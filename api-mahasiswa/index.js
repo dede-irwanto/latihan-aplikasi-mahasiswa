8a3f73035e1cf87dc0be0dc3092bc8e4b2a239a8
Dede Irwanto <52788483+dede-irwanto@users.noreply.github.com>
2022-02-03 06:37:44 +0800

    import body parser

--- api-mahasiswa/index.js
+++ api-mahasiswa/index.js
@@ -2,6 +2,15 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
+// import body parser
+const bodyParser = require("body-parser");
+
+// parse aplication/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse aplication/json
+app.use(bodyParser.json());
+
 // import route mahasiswa
 const mahasiswaRouter = require("./routes/mahasiswa");
 app.use("/api/mahasiswa", mahasiswaRouter);
