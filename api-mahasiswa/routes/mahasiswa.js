2929ea0c733f35373059670fda07fac5c28bbc4f
Dede Irwanto <52788483+dede-irwanto@users.noreply.github.com>
2022-02-03 22:47:33 +0800

    update mahasiswa.js: add get mahasiswa by id, update & delete

--- api-mahasiswa/routes/mahasiswa.js
+++ api-mahasiswa/routes/mahasiswa.js
@@ -74,4 +74,123 @@ router.post(
     });
   }
 );
+
+/**
+ * GET MAHASISWA BY ID
+ */
+router.get("/:id", (req, res) => {
+  let id = req.params.id;
+
+  // query
+  connection.query(
+    `SELECT * FROM mahasiswa WHERE id_mahasiswa = ${id}`,
+    (err, rows) => {
+      if (err) {
+        return res.status(500).json({
+          status: false,
+          message: "Internal Server Error",
+        });
+      }
+
+      // if post not found
+      if (rows.length <= 0) {
+        return res.status(404).json({
+          status: false,
+          message: "Data Mahasiswa Not Found",
+        });
+      } else {
+        return res.status(200).json({
+          status: true,
+          message: "Detail Data Mahasiswa",
+          data: rows[0],
+        });
+      }
+    }
+  );
+});
+
+/**
+ * UPDATE MAHASISWA
+ */
+router.patch(
+  "/update/:id",
+  [
+    // validation
+    body("nim").notEmpty(),
+    body("nama").notEmpty(),
+    body("jurusan").notEmpty(),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({
+        errors: errors.array(),
+      });
+    }
+
+    // id mahasiswa
+    let id = req.params.id;
+
+    // data mahasiswa
+    let formData = {
+      nim: req.body.nim,
+      nama: req.body.nama,
+      jurusan: req.body.jurusan,
+    };
+
+    // update query
+    connection.query(
+      `UPDATE mahasiswa SET ? WHERE id_mahasiswa = ${id}`,
+      formData,
+      (err, rows) => {
+        // if error throw err
+        if (err) {
+          return res.status(500).json({
+            status: false,
+            message: "Internanl Server Error",
+          });
+        }
+
+        if (rows.length <= 0) {
+          return res.status(404).json({
+            status: false,
+            message: "Data Mahasiswa Not Found",
+          });
+        } else {
+          return res.status(200).json({
+            status: true,
+            message: "Update Data Successfully",
+          });
+        }
+      }
+    );
+  }
+);
+
+/**
+ * DELETE MAHASISWA
+ */
+router.delete("/delete/:id", (req, res) => {
+  let id = req.params.id;
+
+  // delete query
+  connection.query(
+    `DELETE FROM mahasiswa WHERE id_mahasiswa = ${id}`,
+    (err, rows) => {
+      // if error throw err
+      if (err) {
+        return res.status(500).json({
+          status: false,
+          message: "Internal Server Error",
+        });
+      } else {
+        return res.status(200).json({
+          status: true,
+          message: "Delete Mahasiswa Successfully",
+        });
+      }
+    }
+  );
+});
 module.exports = router;
