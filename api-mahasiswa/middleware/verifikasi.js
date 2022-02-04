d1f7d1eae006a9afcc1a095363d0a56a03cf6d60
Dede Irwanto <52788483+dede-irwanto@users.noreply.github.com>
2022-02-04 23:35:18 +0800

    add verifikasi.js

---
+++ api-mahasiswa/middleware/verifikasi.js
@@ -0,0 +1,46 @@
+const jwt = require("jsonwebtoken");
+const config = require("../config/secret");
+
+const verifikasi = () => {
+  return (req, res, next) => {
+    //   cek authorization header
+    var bearerToken = req.headers.authorization;
+
+    if (bearerToken) {
+      var token = bearerToken.split(" ")[1];
+
+      //   verifikasi
+      jwt.verify(token, config.secret, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({
+            auth: false,
+            message: "Token tidak terdaftar",
+          });
+        } else {
+          // get user role
+          var role = Buffer.from(token.split(".")[1], "base64")
+            .toString("utf8")
+            .split(",")[2]
+            .split(":")[1];
+
+          if (role == 2) {
+            req.auth = decoded;
+            next();
+          } else {
+            return res.status(401).send({
+              auth: false,
+              message: "Gagal mengotorisasi role anda",
+            });
+          }
+        }
+      });
+    } else {
+      return res.status(401).send({
+        auth: false,
+        message: "Token tidak tersedia",
+      });
+    }
+  };
+};
+
+module.exports = verifikasi;
